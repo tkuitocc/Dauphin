08dbb9d2a2358a5e0c2ac5f3fcf763efeb6220d8
u8b19 <bochainwu@gmail.com>
2024-11-14 17:27:49 +0800

    Initial Commit

---
+++ campuspass_ios/campuspass_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  campuspass_iosApp.swift
+//  campuspass_ios
+//
+//  Created by \u8b19 on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct campuspass_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
