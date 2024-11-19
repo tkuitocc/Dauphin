c78140dd986e1463cfa3d4ae2d028e16f43c45e7
u8b19 <bochainwu@gmail.com>
2024-11-19 22:33:08 +0800

    feat: 根據今天的日期自動選擇今天的課表 支援深色模式

--- campuspass_ios/View/CourseCardView.swift
+++ campuspass_ios/View/CourseCardView.swift
@@ -10,38 +10,47 @@ struct CourseCardView: View {
     let courseName: String
     let roomNumber: String
     let teacherName: String
-    let time: String
+    let StartTime: String
+    let EndTime: String
     let stdNo: String
 
-    var body: some View {
-        HStack() {
-            VStack(alignment: .leading, spacing: 8) {
-                Text(courseName)
-                    .font(.headline)
-                Text("\(roomNumber)\n\(teacherName)")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                
+    @Environment(\.colorScheme) var colorScheme
+
+        var body: some View {
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(courseName)
+                        .font(.headline)
+                        .foregroundColor(colorScheme == .dark ? .orange : .blue)
+                    Text("\(roomNumber)\n\(teacherName)")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("\(StartTime) ~ \(EndTime)")
+                        .font(.footnote)
+                        .foregroundColor(colorScheme == .dark ? .orange : .green)
+                        .padding(.bottom, 20)
+                    
+                    Text("\(stdNo)")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                }
             }
-            Spacer()
-            VStack(alignment: .trailing) {
-                Text(time)
-                    .font(.footnote)
-                    .foregroundColor(.blue)
-                    .padding(.bottom, 20)
-                
-                Text("\(stdNo)")
-                    .font(.footnote)
-                    .foregroundColor(.gray)
-            }
+            .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(colorScheme == .dark ? Color.black.opacity(0.5) : Color.white)
+                .cornerRadius(10)
+                .overlay(
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(colorScheme == .dark ? Color.gray.opacity(0.5) : Color.gray.opacity(0.3)),
+                    alignment: .bottom
+                )
         }
-        .padding()
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .background(Color.gray.opacity(0.1))
-        .cornerRadius(10)
-    }
 }
 
 #Preview {
-    CourseCardView(courseName: "計算機組織", roomNumber: "E305", teacherName: "我", time: "11:10 - 12:10", stdNo: "178")
+    CourseCardView(courseName: "計算機組織", roomNumber: "E305", teacherName: "我", StartTime: "8:10", EndTime: "9:00" , stdNo: "178")
 }
