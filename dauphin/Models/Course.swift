29dd859b914786bce2b9c9fb3d4cbcd3f219019f
u8b19 <bochainwu@gmail.com>
2024-12-11 16:39:13 +0800

    Update getNextUpCourses

--- dauphin/Models/Course.swift
+++ dauphin/Models/Course.swift
@@ -19,10 +19,44 @@ struct Course: Identifiable, Hashable, Codable {
     var weekday: Int
 }
 
-func getNextUpCourse(from weeklyschedule: [Course]) -> Course? {
-    return weeklyschedule[0]
+func getNextUpCourses(from weeklySchedule: [Course]) -> [Course] {
+    let calendar = Calendar.current
+    let now = Date()
+    
+    let todayWeekday = calendar.component(.weekday, from: now)
+    
+    let sortedCourses = weeklySchedule.sorted { course1, course2 in
+        if course1.weekday != course2.weekday {
+            return course1.weekday < course2.weekday
+        } else {
+            let (endHour1, endMinute1) = getCourseEndHourMinute(course1)
+            let (endHour2, endMinute2) = getCourseEndHourMinute(course2)
+            
+            let endDate1 = calendar.date(bySettingHour: endHour1, minute: endMinute1, second: 0, of: now)!
+            let endDate2 = calendar.date(bySettingHour: endHour2, minute: endMinute2, second: 0, of: now)!
+            
+            return endDate1 < endDate2
+        }
+    }
+    
+    if todayWeekday == 7 {
+        return sortedCourses
+    }
+    
+    let upcomingCourses = sortedCourses.filter { course in
+        if course.weekday < todayWeekday {
+            return false
+        } else if course.weekday == todayWeekday {
+            let (endHour, endMinute) = getCourseEndHourMinute(course)
+            let courseEndDate = calendar.date(bySettingHour: endHour, minute: endMinute, second: 0, of: now)!
+            let timeDifference = courseEndDate.timeIntervalSince(now)
+            return timeDifference > 20 * 60
+        } else {
+            return true
+        }
+    }
+    return upcomingCourses
 }
-
 
 func getCourseStartHourMinute(_ course: Course) -> (hour: Int, minute: Int) {
     let calendar = Calendar.current
