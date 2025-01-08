3e7b49bbe6e8de1e95567bc67921c8bb29f77e37
u8b19 <bochainwu@gmail.com>
2025-01-08 17:27:03 +0800

    feat: add code39 for library

--- dauphin/View/Library/LibraryView.swift
+++ dauphin/View/Library/LibraryView.swift
@@ -6,11 +6,23 @@
 //
 
 import SwiftUI
+import Code39
 
 struct LibraryView: View {
     @ObservedObject var authViewModel: AuthViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if authViewModel.isLoggedIn {
+            ScrollView{
+                VStack {
+                    Text("圖書館個人條碼")
+                    Code39View("\(authViewModel.ssoStuNo)")
+                        .frame(width: 296, height: 96)
+                    Text("學號：\(authViewModel.ssoStuNo)")
+                }
+            }
+        } else {
+            LibSSOLoginView(viewModel: authViewModel)
+        }
     }
 }
 
