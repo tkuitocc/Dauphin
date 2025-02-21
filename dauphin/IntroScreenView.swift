a1006bdab2c243f6229be80c65e9de26acbafd3c
u8b19 <bochainwu@gmail.com>
2025-02-21 21:22:26 +0800

    style: add i18n

--- dauphin/IntroScreenView.swift
+++ dauphin/IntroScreenView.swift
@@ -20,17 +20,57 @@ struct InrtoScreen: View {
             
             //Points View
             VStack(alignment: .leading, spacing: 25,content: {
-                PointView(symbol: "laptopcomputer.trianglebadge.exclamationmark", title: "Open Source", subtitle: "我們會開放我們的原始碼，如果你對現在的程式感到不滿意歡迎自己到GitHub上改。")
-                
-                PointView(symbol: "list.bullet.rectangle", title: "Local First", subtitle: "一些重複的資料我們會用安全的方式存在本地端，讓你在連不到學校伺服器也可以知道課表。")
-                
-                PointView(symbol: "widget.large.badge.plus", title: "iOS Feature Support", subtitle: "我們支援了很多iOS的功能像Widget，我們心情好的話也會持續更新")
+                HStack(spacing: 20) {
+                    Image(systemName: "laptopcomputer.trianglebadge.exclamationmark")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.blue.gradient)
+                        .frame(width: 40)
+                    VStack(alignment: .leading,spacing: 6, content: {
+                        Text("Open Source")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("We're open-sourcing our code. If you're not satisfied with the current version, feel free to fork it on GitHub.")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    })
+                }
+                HStack(spacing: 20) {
+                    Image(systemName: "list.bullet.rectangle")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.blue.gradient)
+                        .frame(width: 40)
+                    VStack(alignment: .leading,spacing: 6, content: {
+                        Text("Local First")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("We securely cache some redundant data locally, so you can still access your schedule without connecting to the school's server.")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    })
+                }
+                HStack(spacing: 20) {
+                    Image(systemName: "widget.large.badge.plus")
+                        .font(.largeTitle)
+                        .foregroundStyle(Color.blue.gradient)
+                        .frame(width: 40)
+                    VStack(alignment: .leading,spacing: 6, content: {
+                        Text("iOS Feature Support")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        
+                        Text("We support many iOS features like Widgets, and we'll update further as inspiration strikes.")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    })
+                }
             })
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.horizontal, 15)
             
             Spacer(minLength: 10)
-            Text("⚠️本程式並非由淡江大學資訊處開發，使用者必須自行承擔造成的所有後果")
+            Text("⚠️This app isn’t developed by Tamkang University’s IT Office. Use at your own risk.")
                 .font(.system(size: 8))
             
             Button(action: {
@@ -46,25 +86,6 @@ struct InrtoScreen: View {
             })
         }
         .padding(15)
-    }
-    
-    @ViewBuilder
-    func PointView(symbol: String, title: String, subtitle: String) -> some View {
-        HStack(spacing: 20) {
-            Image(systemName: symbol)
-                .font(.largeTitle)
-                .foregroundStyle(Color.blue.gradient)
-                .frame(width: 40)
-            VStack(alignment: .leading,spacing: 6, content: {
-                Text(title)
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                
-                Text(subtitle)
-                    .font(.caption)
-                    .foregroundStyle(.gray)
-            })
-        }
     }
 }
 
